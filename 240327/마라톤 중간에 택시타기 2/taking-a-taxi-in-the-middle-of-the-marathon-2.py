ec7f6484b1e38eb8cc1b8758c9d1c7ab66c82a89
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-03-27 14:03:28 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240327/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -5,19 +5,22 @@ min_val = sys.maxsize
 n = int(input())
 point = [list(map(int, input().split())) for _ in range(n)]
 
+def calc(x, y):
+    return abs(x[0] - y[0]) + abs(x[1] - y[1])
+
 total = 0
 for i in range(1, n):
     prev = point[i-1]
     curr = point[i]
 
-    total += abs(curr[0] - prev[0]) + abs(curr[1] - prev[1])
+    total += calc(curr, prev)
 
 for i in range(1, n-1):
     prev = point[i-1]
     curr = point[i]
     next = point[i+1]
     
-    tmp = total - (abs(curr[0] - prev[0]) + abs(curr[1] - prev[1])) - (abs(next[0] - curr[0]) + abs(next[1] - curr[1])) + (abs(next[0] - prev[0] + abs(next[1] - prev[1])))
+    tmp = total - calc(curr, prev) - calc(next, curr) + calc(next, prev)
     
     min_val = min(min_val, tmp)
 
