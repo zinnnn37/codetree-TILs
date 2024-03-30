8825c2b7b5e58d31a25d081360857c44a4cc0325
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-03-30 16:15:30 +0900

    Title: 트로미노, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240330/트로미노/tromino.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+nums = [list(map(int, input().split())) for _ in range(n)]
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+res = 0
+
+# 3*1 1*3
+for i in range(n):
+    for j in range(n-2):
+        res = max(res, sum(nums[i][j:j+3]))
+
+        tmp = 0
+        for k in range(3):
+            tmp += nums[j+k][i]
+        res = max(res, tmp)
+    
+
+for i in range(n):
+    for j in range(n):
+        for k in range(4):
+            nx = i + dx[k]
+            ny = j + dy[k]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                res = max(res, nums[i][j] + nums[nx][ny] + nums[dx[(nx + 1) % 4]][dy[(ny + 1) % 4]])
+
+print(res)
