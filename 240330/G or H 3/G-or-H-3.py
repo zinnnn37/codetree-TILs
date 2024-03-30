8456da981378ce5a22d29eaf0f40cbe9add773ba
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-03-30 13:38:16 +0900

    Title: G or H 3, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240330/G or H 3/G-or-H-3.py
@@ -0,0 +1,28 @@
+n, k = map(int, input().split())
+lst = []
+
+for _ in range(n):
+    a, b = input().split()
+    lst.append([int(a), b])
+
+lst.sort(key=lambda x : x[0])
+
+def GH(c):
+    return 1 if c == 'G' else 2
+
+left = 0
+right = 0
+res = 0
+tmp = 0
+while (left < n and right < n):
+    if lst[right][0] - lst[left][0] > k:
+        left += 1
+        tmp -= GH(lst[left][1])
+        res = max(res, tmp)
+        continue
+
+    tmp += GH(lst[right][1])
+    res = max(res, tmp)
+    right += 1
+
+print(res)
