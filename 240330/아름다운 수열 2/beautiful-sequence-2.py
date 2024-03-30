39f921462c143ac2cf4e4e58c4a618379a83fba0
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-03-30 14:10:31 +0900

    Title: 아름다운 수열 2, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240330/아름다운 수열 2/beautiful-sequence-2.py
+++ 240330/아름다운 수열 2/beautiful-sequence-2.py
@@ -2,12 +2,17 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
+b.sort()
+
 cnt = 0
 for i in range(n-m+1):
+    tmp = a[i:i+m]
+    tmp.sort()
     for j in range(m):
-        if a[i+j] not in b:
+        if tmp[j] != b[j]:
             break
-        if j == m-1 and a[i+j] in b:
+        
+        if j == m-1 and tmp[j] == b[j]:
             cnt += 1
 
 print(cnt)
