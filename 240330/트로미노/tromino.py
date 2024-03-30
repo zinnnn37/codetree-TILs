cafc0f17ed71fafe8d92883a05b0acaa19ec242b
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-03-30 16:30:55 +0900

    Title: 트로미노, Time: 170ms, Memory: 25MB, Status: Passed - CodeTree

--- 240330/트로미노/tromino.py
+++ 240330/트로미노/tromino.py
@@ -1,31 +1,31 @@
 n, m = map(int, input().split())
 nums = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+di = [
+    [[0, 0], [0, 1], [0, 2]],   # 가로
+    [[0, 0], [1, 0], [2, 0]],   # 세로
+    [[0, 0], [0, 1], [1, 0]],   # ⌜
+    [[0, 0], [0, 1], [-1, 0]],  # ⌞
+    [[0, 0], [0, -1], [1, 0]],  # ⌝
+    [[0, 0], [0, -1], [-1, 0]], # ⌟
+]
+
+def get_sum(i, j, d):
+    tmp = 0
+    for dx, dy in d:
+        nx = i + dx
+        ny = j + dy
+
+        if 0 <= nx < n and 0 <= ny < m:
+            tmp += nums[nx][ny]
+        else: return 0
+
+    return tmp
 
 res = 0
-
-# 3*1 1*3
-for i in range(n):
-    for j in range(m-2):
-        res = max(res, sum(nums[i][j:j+3]))
-
-for i in range(n-2):
-    for j in range(m):
-        tmp = 0
-        for k in range(3):
-            tmp += nums[i+k][j]
-        res = max(res, tmp)
-    
-
 for i in range(n):
     for j in range(m):
-        for k in range(4):
-            nx = i + dx[k]
-            ny = j + dy[k]
-
-            if 0 <= nx < n and 0 <= ny < m:
-                res = max(res, nums[i][j] + nums[nx][ny] + nums[dx[(nx + 1) % 4]][dy[(ny + 1) % 4]])
+        for d in di:
+            res = max(res, get_sum(i, j, d))
 
 print(res)
