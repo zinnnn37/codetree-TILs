744e4c4fbe01c11bc4d1a379c402298768d9176c
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-03-30 11:04:11 +0900

    Title: 최고의 13위치 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240330/최고의 13위치 2/best-place-of-13-2.py
+++ 240330/최고의 13위치 2/best-place-of-13-2.py
@@ -1,22 +1,21 @@
 n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
 
+res = 0
+
 def add(i, j):
     return sum(matrix[i][j:j+3])
 
-r1 = add(0, 0)
-r2 = add(0, 1)
-
 for i in range(n):
     for j in range(n-2):
-        if i == 0 and (j == 0 or j == 1):
-            continue
-        
         tmp = add(i, j)
 
-        if tmp > r1:
-            r1 = tmp
-        elif tmp > r2:
-            r2 = tmp
+        for a in range(i, n):
+            for b in range(n-2):
+                if a == i and b <= j+2:
+                    continue
+                
+                tmp += add(a, b)
+                res = max(res, tmp)
 
-print(r1 + r2)
+print(res)
