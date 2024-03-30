38d21cd7a7d3097d429f53b1e648c469a45dbeaa
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-03-30 11:05:21 +0900

    Title: 최고의 13위치 2, Time: 285ms, Memory: 28MB, Status: Passed - CodeTree

--- 240330/최고의 13위치 2/best-place-of-13-2.py
+++ 240330/최고의 13위치 2/best-place-of-13-2.py
@@ -8,14 +8,13 @@ def add(i, j):
 
 for i in range(n):
     for j in range(n-2):
-        tmp = add(i, j)
 
         for a in range(i, n):
             for b in range(n-2):
                 if a == i and b <= j+2:
                     continue
                 
-                tmp += add(a, b)
+                tmp = add(i, j) + add(a, b)
                 res = max(res, tmp)
 
 print(res)
