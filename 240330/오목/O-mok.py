f12597b8cd343ed0edb1fb3edb2a56b6c880b39f
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-03-30 10:51:19 +0900

    Title: 오목, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240330/오목/O-mok.py
+++ 240330/오목/O-mok.py
@@ -1,5 +1,4 @@
 board = [list(map(int, input().split())) for _ in range(19)]
-visited = [[False for _ in range(19)] for __ in range(19)]
 
 dx = [0, 1, 0, -1, -1, -1, 1, 1]
 dy = [1, 0, -1, 0, -1, 1, -1, 1]
@@ -11,10 +10,8 @@ def go(x, y, color, cnt, dx, dy):
     nx = x + dx
     ny = y + dy
 
-    if 0 <= nx < 19 and 0 <= ny < 19 and visited[nx][ny] == False and board[nx][ny] == color:
-        visited[nx][ny] = True
+    if 0 <= nx < 19 and 0 <= ny < 19 and board[nx][ny] == color:
         ret = go(nx, ny, color, cnt+1, dx, dy)
-        visited[nx][ny] = False
         return ret
     
     return False
@@ -24,10 +21,8 @@ def find(x, y, color, cnt):
         nx = x + dx[n]
         ny = y + dy[n]
 
-        if 0 <= nx < 19 and 0 <= ny < 19 and visited[nx][ny] == False and board[nx][ny] == color:
-            visited[nx][ny] = True
+        if 0 <= nx < 19 and 0 <= ny < 19 and board[nx][ny] == color:
             ret = go(nx, ny, color, cnt+1, dx[n], dy[n])
-            visited[nx][ny] = False
             return ret
 
     return False
@@ -36,14 +31,10 @@ black = False
 white = False
 for i in range(19):
     for j in range(19):
-        visited[i][j] = True
-
         if board[i][j] == 1:
             black = find(i, j, 1, 1)
         elif board[i][j] == 2:
             white = find(i, j, 2, 1)
-        
-        visited[i][j] = False
         
         if black or white:
             break
