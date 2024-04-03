428d1c78327127e76171ac16a6929d5ca4876044
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-04-03 14:34:53 +0900

    Title: 금 채굴하기, Time: 309ms, Memory: 27MB, Status: Passed - CodeTree

--- 240403/금 채굴하기/gold-mining.py
+++ 240403/금 채굴하기/gold-mining.py
@@ -6,7 +6,11 @@ gold = 0
 def count_gold(x, y, k):
     cnt = 0
     for i in range(-k, k+1):
-        for j in range(-k + abs(i), k - abs(i) + 1): # 다이아몬드 모양으로 탐색
+        a = -k + abs(i)
+        b = k - abs(i)
+        step = b - a if a != b else 1
+
+        for j in range(a, b + 1, step): # 다이아몬드 모양으로 탐색
             nx = x + i
             ny = y + j
 
@@ -16,8 +20,8 @@ def count_gold(x, y, k):
 
 for i in range(n):
     for j in range(n):
+        cnt = 0
         for k in range(n * 2 - 1):
-            cnt = 0
             cost = k * k + (k+1) * (k+1)
 
             cnt += count_gold(i, j, k)
