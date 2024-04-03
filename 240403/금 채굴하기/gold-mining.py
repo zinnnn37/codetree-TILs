019f00bb52c98308beb97e920153b54a226e21c9
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-04-03 14:22:59 +0900

    Title: 금 채굴하기, Time: 570ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240403/금 채굴하기/gold-mining.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+mine = [list(map(int, input().split())) for _ in range(n)]
+
+gold = 0
+
+def count_gold(x, y, k):
+    cnt = 0
+    for i in range(-k, k+1):
+        for j in range(-k + abs(i), k - abs(i) + 1):
+            nx = x + i
+            ny = y + j
+
+            if 0 <= nx < n and 0 <= ny < n and mine[nx][ny] == 1:
+                cnt += 1
+    return cnt
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n * 2 - 1):
+            visited = [['.' for _ in range(n)] for _ in range(n)]
+            cnt = 0
+            cost = k * k + (k+1) * (k+1)
+            cnt += count_gold(i, j, k)
+
+            if cnt * m >= cost:
+                gold = max(gold, cnt)
+
+print(gold)
