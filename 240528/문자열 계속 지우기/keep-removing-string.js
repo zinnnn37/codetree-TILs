ed6aeccd1acb68262133a677ca67a1ad97b053f8
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-05-28 20:46:20 +0900

    Title: 문자열 계속 지우기, Time: 235ms, Memory: 47MB, Status: Passed - CodeTree

--- 240528/문자열 계속 지우기/keep-removing-string.js
+++ 240528/문자열 계속 지우기/keep-removing-string.js
@@ -2,11 +2,10 @@ const fs = require('fs');
 let [haystack, needle] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const needleLength = needle.length;
 
-for (let i = 0; i < haystack.length; i++) {
-    if (haystack.slice(i, i+needleLength) === needle) {
-        haystack = haystack.slice(0, i) + haystack.slice(i+needleLength);
-        i -= 2;
-    }
+let indexOfNeedle = haystack.indexOf(needle);
+while (indexOfNeedle != -1) {
+    haystack = haystack.slice(0, indexOfNeedle) + haystack.slice(indexOfNeedle + needleLength);
+    indexOfNeedle = haystack.indexOf(needle);
 }
 
 console.log(haystack);
