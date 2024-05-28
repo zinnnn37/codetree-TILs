d509d52efa664ce32ea462192bba30ed87237176
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-05-28 17:53:15 +0900

    Title: 격자 반대로 채우기, Time: 193ms, Memory: 47MB, Status: Passed - CodeTree

--- 240528/격자 반대로 채우기/grid-anti-fill.js
+++ 240528/격자 반대로 채우기/grid-anti-fill.js
@@ -5,8 +5,7 @@ let cnt = 1;
 let res = Array(n).fill(0).map(() => Array(n).fill(0));
 for (let j = n-1; j >= 0; j--) {
     for (let i = n-1; i >= 0; i--) {
-        // odd column
-        if (j % 2 == 1) {
+        if ((n - j) % 2 == 1) {
             res[i][j] = cnt;
         } else {
             res[n-i-1][j] = cnt;
