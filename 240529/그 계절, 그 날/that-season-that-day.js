f514d2a3fa743613b62dbbaa9342a3ca29c83d3e
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-05-29 19:13:34 +0900

    Title: 그 계절, 그 날, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240529/그 계절, 그 날/that-season-that-day.js
+++ 240529/그 계절, 그 날/that-season-that-day.js
@@ -20,11 +20,12 @@ function checkValid(year, month, day) {
 
     switch (month) {
         case 1, 3, 5, 7, 8, 10, 12:
-            console.log('31');
-            if (div === 0 || month === 12) res = 'Winter';
-            else if (div === 1) res = 'Spring';
-            else if (div === 2) res = 'Summer';
-            else if (div === 3) res = 'Fall';
+            if (day <= 31) {
+                if (div === 0 || month === 12) res = 'Winter';
+                else if (div === 1) res = 'Spring';
+                else if (div === 2) res = 'Summer';
+                else if (div === 3) res = 'Fall';
+            }
             break;
         case 4, 6, 9, 11:
             if (day <= 30) {
