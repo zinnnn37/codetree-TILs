9f1e5ca1ea625cff524ee8f04439f562db315743
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2024-05-29 19:21:30 +0900

    Title: 그 계절, 그 날, Time: 233ms, Memory: 14MB, Status: Passed - CodeTree

--- 240529/그 계절, 그 날/that-season-that-day.js
+++ 240529/그 계절, 그 날/that-season-that-day.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const [year, month, day] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 
-function isLeafYear(year) {
+function isLeapYear(year) {
     if (year % 4 === 0) {
         if (year % 100 === 0) {
             if (year % 400 === 0) {
                 return true;
             }
-            return false ;
+            return false;
         }
         return true;
     }
@@ -16,32 +16,20 @@ function isLeafYear(year) {
 
 function checkValid(year, month, day) {
     let res = -1;
-    const div = month / 3;
+    const daysOfMonth = [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    switch (month) {
-        case 1, 3, 5, 7, 8, 10, 12:
-            if (day <= 31) {
-                if (div === 0 || month === 12) res = 'Winter';
-                else if (div === 1) res = 'Spring';
-                else if (div === 2) res = 'Summer';
-                else if (div === 3) res = 'Fall';
-            }
-            break;
-        case 4, 6, 9, 11:
-            if (day <= 30) {
-                if (div === 1) res = 'Spring';
-                else if (div === 2) res = 'Summer';
-                else res = 'Fall';
-            }
-            break;
-        case 2:
-            if ((isLeafYear(year) && day === 29) || day <= 28) {
-                res = 'Winter'
-            };
-            break;
-        default:
-            res = -1;
+    if (day > daysOfMonth[month - 1]) return res;
+    
+    if (month === 1 || month === 2 || month === 12) {
+        res = 'Winter';
+    } else if (month >= 3 && month <= 5) {
+        res = 'Spring';
+    } else if (month >= 6 && month <= 8) {
+        res = 'Summer';
+    } else if (month >= 9 && month <= 11) {
+        res = 'Fall';
     }
+    
     return res;
 }
 
